4feb375ccd252e5e8e5627df5cb14c4c6fe2feb5
xPamii <pbherath2018@gmail.com>
2026-02-06 10:00:38 +0530

    feat: Implement notification functionality
    This commit adds the necessary code to request notification permissions and send a basic notification.
    
    - A notification channel is created on devices running Android 8.0 (Oreo) and higher.
    - A permission request launcher is registered to handle the `POST_NOTIFICATIONS` permission on Android 13 (Tiramisu) and newer.
    - A `sendNotification` method is added to build and display a notification when the "Notification" button is clicked.
    - The `onCreate` method is updated to initialize the permission check and set the click listener for the notification button.

--- app/src/main/java/com/xpamii/notification/activity/MainActivity.java
+++ app/src/main/java/com/xpamii/notification/activity/MainActivity.java
@@ -26,6 +26,20 @@ import com.xpamii.notification.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "my_channel_01";
+
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> { // Request permission launcher.
+
+                if (isGranted) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +53,88 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //Check if notification permission is granted or not.
+        //If not, request permission.
+        //If yes, create notification channel.
+        createNotificationChannel();
+        checkAndRequestNotificationPermission();
+
+        Button notificationButton = findViewById(R.id.notification_button);
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+
+        });
+
     }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle("Notification Title")
+                .setContentText("Notification Body")
+                .setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);//
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            int notificationId = 1;
+            managerCompat.notify(notificationId, builder.build());
+        } else {
+            checkAndRequestNotificationPermission();
+        }
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Our android OS version >= Android Version 8 (Oreo).
+            CharSequence name = " General Channel"; // The user-visible name of the channel.
+            String description = "Channel for general notifications"; // The user-visible description of the channel.
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance); // Create notification channel.
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);// Get notification manager.
+            notificationManager.createNotificationChannel(channel); // Create notification channel.
+
+        }
+
+    }
+
+    private void checkAndRequestNotificationPermission() { // Check if permission is granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {// Our android OS version >= Android Version 13 (TIRAMISU).
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {// If permission is not granted.
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);// Request permission.
+            }
+        }
+    }
+
+
+//    private static final int NOTIFICATION_NUMBER = 1;
+//
+//    private void popupNotification() {
+//        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        String channelId = "channel_id";
+//        NotificationChannel testNotification = new NotificationChannel( channelId,
+//                "Test Notification",
+//                NotificationManager.IMPORTANCE_DEFAULT);
+//        manager.createNotificationChannel(testNotification);
+//
+//        Notification notification = new NotificationCompat.Builder(this, channelId)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle("Notification Practical")
+//                .setContentText("This is my notification manager")
+//                .setAutoCancel(true)
+//                .build();
+//
+//        manager.notify(NOTIFICATION_NUMBER, notification);
+//
+//    }
+
 }
 
 
