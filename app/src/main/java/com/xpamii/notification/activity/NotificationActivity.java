f25cd9a0542cf39184650ef11a7d6514734abe23
xPamii <pbherath2018@gmail.com>
2026-02-06 10:36:11 +0530

    feat: Handle notification remote input
    This commit adds functionality to the `NotificationActivity` to process remote input from a notification.
    
    - It retrieves the text input from the intent using `RemoteInput.getResultsFromIntent`.
    - The received text is then displayed on the screen as a `Toast` message.

--- app/src/main/java/com/xpamii/notification/activity/NotificationActivity.java
+++ app/src/main/java/com/xpamii/notification/activity/NotificationActivity.java
@@ -1,9 +1,11 @@
 package com.xpamii.notification.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.RemoteInput;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,5 +24,12 @@ public class NotificationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Bundle bundle = RemoteInput.getResultsFromIntent(getIntent());
+        if (bundle != null){
+            CharSequence charSequence =  bundle.getCharSequence(MainActivity.KEY_TEXT_REPLY);
+            Toast.makeText(this, charSequence, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
